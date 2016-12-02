178f261826f57aef889050b2dc23ad27506320f2
Ethan Rucker <rucker95@berkeley.edu>
2016-12-02 23:56:26 +0000

    setup for notifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161202231023) do
+ActiveRecord::Schema.define(version: 20161202235333) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "username"
@@ -62,6 +62,9 @@ ActiveRecord::Schema.define(version: 20161202231023) do
   create_table "notifications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "action"
+    t.string   "post"
+    t.integer  "action_id"
   end
 
   create_table "points", force: :cascade do |t|
@@ -123,18 +126,18 @@ ActiveRecord::Schema.define(version: 20161202231023) do
   add_index "taggedcomments", ["tagged_post_id"], name: "index_taggedcomments_on_tagged_post_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",        null: false
-    t.string   "encrypted_password",     default: "",        null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,         null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
     t.string   "username"
     t.string   "provider"
@@ -145,7 +148,7 @@ ActiveRecord::Schema.define(version: 20161202231023) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.boolean  "spanish",                default: false
-    t.string   "location",               default: "Unknown"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
